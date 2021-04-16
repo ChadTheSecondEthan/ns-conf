91ce60fbec403a24a3c528be6f485c218f68c105
ChadTheSecondEthan <fishere@franklinacademy.org>
2021-04-16 16:43:55 -0400

    First Commit

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import 'react-native-gesture-handler';
-import Navigation from './Navigation';
-import StorageProvider from './storage/StorageProvider';
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import "react-native-gesture-handler";
+import Navigation from "./Navigation";
+import StorageProvider from "./storage/StorageProvider";
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <StorageProvider>
-                <Navigation />
-            </StorageProvider>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <StorageProvider>
+        <Navigation />
+      </StorageProvider>
+    </NavigationContainer>
+  );
 }
--- storage/service.js
+++ storage/service.js
@@ -1,83 +1,86 @@
-import axios from 'axios';
-import { AsyncStorage } from 'react-native';
-import { apply, asyncPipe } from '../utils/pipe';
+import axios from "axios";
+import { AsyncStorage } from "@react-native-community/async-storage";
+import { apply, asyncPipe } from "../utils/pipe";
 
 // CONSTANTS
-
 const keys = {
-    timestamp: 'timestamp',
-    schedule: 'schedule',
-    feedback: 'feedback',
-    selections: 'selections',
-    home: 'home',
+  timestamp: "timestamp",
+  schedule: "schedule",
+  feedback: "feedback",
+  selections: "selections",
+  home: "home",
 };
 
 Object.entries(keys).forEach(([key, value]) => {
-    if (key !== value) throw new Error(`Key ${key} !== value ${value}`);
+  if (key !== value) throw new Error(`Key ${key} !== value ${value}`);
 });
 
-const validateKey = key => {
-    if (key in keys) return key;
-    else throw new Error(`Invalid key: ${key}`);
-}
+const validateKey = (key) => {
+  if (key in keys) return key;
+  else throw new Error(`Invalid key: ${key}`);
+};
 
 // UTILS
 
-const getItemFromStorage = key => asyncPipe(
+const getItemFromStorage = (key) =>
+  asyncPipe(
     validateKey(key),
     AsyncStorage.getItem,
     JSON.parse,
-    value => value || undefined,
-);
+    (value) => value || undefined
+  );
 
-const setItemInStorage = (key, value) => asyncPipe(
+const setItemInStorage = (key, value) =>
+  asyncPipe(
     value,
     JSON.stringify,
-    apply(key, AsyncStorage.setItem),
-    () => value || undefined,
-);
+    apply(key, AsyncStore.setItem),
+    () => value || undefined
+  );
 
-const fetchItem = key => asyncPipe(
+const fetchItem = (key) =>
+  asyncPipe(
     axios.get(`http://ns-conf.lowrysoftware.com/api/2/${key}`),
     ({ data }) => data,
-    apply(key, setItemInStorage),
-);
+    apply(key, setItemInStorage)
+  );
 
-const shouldRefetch = key => asyncPipe(
+const shouldRefetch = (key) =>
+  asyncPipe(
     Promise.all([
-        // check timestamp in storage
-        getItemFromStorage(validateKey(key)),
-        // check timestamp of latest update
-        fetchItem(keys.timestamp),
+      // check timestamp in storage
+      getItemFromStorage(key),
+      // check timestamp of latest update
+      fetchItem(keys.timestamp),
     ]),
     // compare timestamps -- ignore anything that was before api version 2
-    ([{ timestamp: storageTimestamp = 0 } = {}, { timestamp = 1 } = {}]) => storageTimestamp < Math.max(1582844213530, timestamp),
-);
+    ([{ timestamp: storageTimestamp = 0 } = {}, { timestamp = 1 } = {}]) =>
+      storageTimestamp < Math.max(1582844213530, timestamp)
+  );
 
-const getItem = key => asyncPipe(
+const getItem = (key) =>
+  asyncPipe(
     shouldRefetch(key),
-    should => should ?
-        fetchItem(key)
-        :
-        getItemFromStorage(key),
+    (should) => (should ? fetchItem(key) : getItemFromStorage(key)),
     apply(key, setItemInStorage),
-    ({ data }) => data,
-);
+    ({ data }) => data
+  );
 
 // ACTIONS
 
-export const getHomeLinks = () => getItem(keys.home)
+export const getHomeLinks = () => getItem(keys.home);
 export const getFeedback = () => getItem(keys.feedback);
 export const getSchedule = () => getItem(keys.schedule);
 export const getSelections = () => getItemFromStorage(keys.selections);
 
 export const hashBreakout = ({ day, time } = {}) => `<${day}><${time}>`;
 
-export const selectOrUnselectBreakout = (select, { day, time, room }) => asyncPipe(
+export const selectOrUnselectBreakout = (select, { day, time, room }) =>
+  asyncPipe(
     getItemFromStorage(keys.selections),
-    selections => ({
-        ...selections,
-        [hashBreakout({ day, time })]: select ? room : undefined,
+    (selections) => ({
+      ...selections,
+      [hashBreakout({ day, time })]: select ? room : undefined,
     }),
     apply(keys.selections, setItemInStorage)
-);
+  );
