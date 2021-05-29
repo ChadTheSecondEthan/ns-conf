cb8184ae3e070ab0f1eddb49ac621650e8454870
ChadTheSecondEthan <fishere@franklinacademy.org>
2021-05-29 16:42:39 -0400

    Updated the schedule with everything but speaker photos

--- components/SessionSpeakers.js
+++ components/SessionSpeakers.js
@@ -1,48 +1,53 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import styles from '../styles/styles';
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "../styles/styles";
 
 export default function SessionSpeakers({
-    session: {
-        speakers = [],
-        panelists = [],
-        moderator,
-        facilitator,
-    } = {},
-    doNotRenderPanelists = false,
-    onlyRenderPanelists = false,
-    style = styles.m3,
+  session: { speakers = [], panelists = [], moderator, facilitator } = {},
+  doNotRenderPanelists = false,
+  onlyRenderPanelists = false,
+  style = styles.m3,
 }) {
-    return (
-        speakers.length
-        ||
-        panelists.length
-        ||
-        moderator
-        ||
-        facilitator
-    ) ? (
-            <View style={styles.marginBottomXxSmall} >
-                {speakers.length && !onlyRenderPanelists ? (
-                    <Text
-                        style={style}
-                    >{speakers.map(({ name }) => name).join(', ')}</Text>
-                ) : null}
-                {facilitator && !onlyRenderPanelists ? (
-                    <Text
-                        style={style}
-                    >Facilitator: {facilitator.name}</Text>
-                ) : null}
-                {moderator && !onlyRenderPanelists ? (
-                    <Text
-                        style={style}
-                    >Moderator: {moderator.name}</Text>
-                ) : null}
-                {panelists.length && !doNotRenderPanelists ? (
-                    <Text
-                        style={style}
-                    >Panelists: {panelists.map(({ name }) => name).join(', ')}</Text>
-                ) : null}
-            </View>
-        ) : null;
+  return speakers.length || panelists.length || moderator || facilitator ? (
+    <View style={styles.marginBottomXxSmall}>
+      {speakers.length && !onlyRenderPanelists ? (
+        <Text style={style}>{speakers.map(({ name }) => name).join(", ")}</Text>
+      ) : null}
+      {facilitator && !onlyRenderPanelists ? (
+        <Text style={style}>{facilitator.name}</Text>
+      ) : null}
+      {moderator && !onlyRenderPanelists ? (
+        <Text style={style}>{moderator.name}</Text>
+      ) : null}
+      {panelists.length && !doNotRenderPanelists ? (
+        <Text style={style}>
+          {panelists.map(({ name }) => name).join(", ")}
+        </Text>
+      ) : null}
+    </View>
+  ) : null;
+  // ) ? (
+  //         <View style={styles.marginBottomXxSmall} >
+  //             {speakers.length && !onlyRenderPanelists ? (
+  //                 <Text
+  //                     style={style}
+  //                 >{speakers.map(({ name }) => name).join(', ')}</Text>
+  //             ) : null}
+  //             {facilitator && !onlyRenderPanelists ? (
+  //                 <Text
+  //                     style={style}
+  //                 >Facilitator: {facilitator.name}</Text>
+  //             ) : null}
+  //             {moderator && !onlyRenderPanelists ? (
+  //                 <Text
+  //                     style={style}
+  //                 >Moderator: {moderator.name}</Text>
+  //             ) : null}
+  //             {panelists.length && !doNotRenderPanelists ? (
+  //                 <Text
+  //                     style={style}
+  //                 >Panelists: {panelists.map(({ name }) => name).join(', ')}</Text>
+  //             ) : null}
+  //         </View>
+  //     ) : null;
 }
